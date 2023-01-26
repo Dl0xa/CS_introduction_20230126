c4f25de5344a713e5f4a416bb1d52244f7660fb9
«dl0xa <dl0xa@mail.ru>
2023-01-26 22:15:24 +0300

    Init commit

---
+++ 000/Program.cs
@@ -0,0 +1,24 @@
+﻿// С клавиатуры вводим два числа. Найти и вывести их сумму
+//string s;
+
+int a, b, sum;
+System.Console.WriteLine("Введите a");
+a = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите b");
+b = Convert.ToInt32(Console.ReadLine());
+sum = a + b;
+//1 Конкатенация (склеивание) строк 
+System.Console.WriteLine(a + "+" + b + "=" + sum);
+//2 Форматированный вывод
+System.Console.WriteLine("{0:F2}+{1:E5}={2:G4}",a, b, sum);
+//3 Интерполяция строк
+System.Console.WriteLine("{a}+{b}={sum}",a, b, sum);
+
+// С клавиатуры вводим два числа. Найти и вывести максимальное из них
+int max;
+if (a>b)
+    max=a;
+else 
+    max=b;
+System.Console.WriteLine($"max={max}");
+
