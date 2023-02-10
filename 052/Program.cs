898493536fa5f59621d1fd393bb6d14744783c08
«dl0xa <dl0xa@mail.ru>
2023-02-10 14:40:17 +0300

    Done after Lessone 5 add

--- 052/Program.cs
+++ 052/Program.cs
@@ -3,16 +3,29 @@
 System.Console.Write("Введите число для преобразования: ");
 int N1 = Convert.ToInt32(Console.ReadLine());
 for (N1 = 0; N1 < 10; N1++)
+
+    // int N = N1;
+    // int N2 = 0;
+    // int k = 0;
+    // while (N != 0)
+
+    // {
+    //     N2 = N2 + N % 2 * (int)Math.Pow(10, k);
+    //     N = N / 2;
+    //     k++;
+    // }
+    System.Console.WriteLine($"Число {N1} в двоичной системе равно: {DecToBin(N1)}");
+
+
+int DecToBin(int N)
 {
-    int N = N1;
     int N2 = 0;
     int k = 0;
     while (N != 0)
-
     {
         N2 = N2 + N % 2 * (int)Math.Pow(10, k);
         N = N / 2;
         k++;
     }
-    System.Console.WriteLine($"Число {N1} в двоичной системе равно: {N2}");
+    return N2;
 }
