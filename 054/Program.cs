1b34e3ec98e5305c869e8c3f6597cdff894685f0
«dl0xa <dl0xa@mail.ru>
2023-02-11 16:26:12 +0300

    Done HW after Lesson #5

---
+++ 054/Program.cs
@@ -0,0 +1,16 @@
+﻿// 54. С клавиатуры вводится число N. Показать первые N чисел Фибоначчи. Принять первые числа равными 0 и 1
+
+System.Console.Write("Введите число, для которого необходима последовательность Фибоначчи: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int F(int n)
+{
+    if (n == 1 || n == 2) return 1;
+    else return F(n - 1) + F(n - 2);
+}
+Console.WriteLine("f(0) = 0");
+{
+    for (int i = 1; i < n; i++)
+    {
+        Console.WriteLine($"f({i}) = {F(i)}");
+    }
+}
---
+++ 056/Program.cs
@@ -0,0 +1,6 @@
+﻿// 56. Написать программу копирования массива
+
+int[] a = { 1, 4, 8, 56, 3, 43, 23, 64, 53 };
+int[] copy = new int[a.Length];
+Array.Copy(a, copy, a.Length);
+System.Console.WriteLine(string.Join(",", copy));
