52ba1e281911259ac2807f8590b533f507659068
«dl0xa <dl0xa@mail.ru>
2023-02-26 18:07:08 +0300

    Done HW After Lesson #9 (93,98)

---
+++ 093/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 93: Задайте две матрицы. Напишите программу, 
+// которая будет находить произведение двух матриц.
+
+int[,] a = new int[4, 4];
+int[,] b = new int[4, 4];
+int[,] c = new int[4, 4];
+PrintAArray(a);
+PrintBArray(b);
+PrintMultiArray(c);
+
+
+
+
+void PrintAArray(int[,] a)
+{
+    Random random = new Random();
+    for (int i = 0; i < a.GetLength(0); i++)
+    {
+        for (int j = 0; j < a.GetLength(1); j++)
+        {
+            a[i, j] = random.Next(20);
+            System.Console.Write($"{a[i, j],4}");
+        }
+        System.Console.WriteLine();
+    }
+    System.Console.WriteLine();
+}
+void PrintBArray(int[,] b)
+{
+    Random random = new Random();
+    for (int i = 0; i < b.GetLength(0); i++)
+    {
+        for (int j = 0; j < b.GetLength(1); j++)
+        {
+            b[i, j] = random.Next(20);
+            System.Console.Write($"{b[i, j],4}");
+        }
+        System.Console.WriteLine();
+    }
+    System.Console.WriteLine("Произведение двух массивов:");
+}
+void PrintMultiArray(int[,] c)
+{
+
+    for (int i = 0; i < a.GetLength(0); i++)
+    {
+        for (int j = 0; j < a.GetLength(1); j++)
+        {
+            c[i, j] = a[i, j] * b[i, j];
+            Console.Write($"{c[i, j],4}");
+        }
+        Console.WriteLine();
+    }
+}
---
+++ 098/Program.cs
@@ -0,0 +1,28 @@
+﻿// 98. Показать треугольник Паскаля. Сделать вывод в виде равнобедренного треугольника.
+
+System.Console.Write("Введите количество строк: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] a = new int[n, n];
+for (int i = 0; i < a.GetLength(0); i++)
+{
+    a[i, 0] = 1;
+    a[i, i] = 1;
+}
+for (int i = 2; i < a.GetLength(0); i++)
+{
+    for (int j = 1; j <= i; j++)
+    {
+        a[i, j] = a[i - 1, j - 1] + a[i - 1, j];
+    }
+}
+
+for (int i = 0; i < a.GetLength(0); i++)
+{
+    for (int j = 0; j < a.GetLength(0); j++)
+    {
+        if (a[i, j] != 0)
+            Console.Write($"{a[i, j],5}");
+    }
+    Console.WriteLine();
+}
